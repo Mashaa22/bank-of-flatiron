9ca554cba5a9e6d7e9503c17706b73a9fa993752
AlexMachariaMaranga <amaranga22@gmail.com>
2023-01-16 01:25:33 +0300

    added components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <br></br>
+    <div className="text-center " >
+            <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      </div>
+      </>
+  );
+}
+
+export default App;
